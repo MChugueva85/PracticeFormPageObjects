a03b9b2693ea35b58bad0a8b8d74a5cb02a5b1eb
MChugueva85 <livingsmiling85@gmail.com>
2023-07-28 10:58:14 +0300

    исправлена ошибка в СalendarComponent

--- src/test/java/home_task_page_object/pages/components/CalendarComponent.java
+++ src/test/java/home_task_page_object/pages/components/CalendarComponent.java
@@ -9,7 +9,7 @@ public class CalendarComponent {
         $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__year-select").selectOption(year);
         $(".react-datepicker__day--0" + day +
-                ":not(.react-datapicker__day--outside-month)").click();
+                ":not(.react-datepicker__day--outside-month)").click();
     }
 
 }
