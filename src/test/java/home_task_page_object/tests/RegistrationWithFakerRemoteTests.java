0fd5b6318fa8e443156098112a6a3e2ccb19a700
MChugueva85 <livingsmiling85@gmail.com>
2023-08-11 16:55:30 +0300

    added Tag("remote")

--- src/test/java/home_task_page_object/tests/RegistrationWithFakerRemoteTests.java
+++ src/test/java/home_task_page_object/tests/RegistrationWithFakerRemoteTests.java
@@ -4,8 +4,11 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import home_task_page_object.pages.RegistrationPageTests;
 import home_task_page_object.pages.components.ResultPageComponent;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+
+@Tag("remote")
 public class RegistrationWithFakerRemoteTests extends TestBaseRemote {
 
 
@@ -16,28 +19,28 @@ public class RegistrationWithFakerRemoteTests extends TestBaseRemote {
         SelenideLogger.addListener("allure", new AllureSelenide());
         RegistrationPageTests registrationPageTests = new RegistrationPageTests();
 
-        registrationPageTests.openPage();
-        registrationPageTests.removeFixedbanFooter();
-        registrationPageTests.setFirstName(randomData.firstName);
-        registrationPageTests.setLastName(randomData.lastName);
-        registrationPageTests.setUserEmail(randomData.userEmail);
-        registrationPageTests.setGender(randomData.gender);
-        registrationPageTests.setUserNumber(randomData.userNumber);
-        registrationPageTests.setBirthDay(randomData.day, randomData.month, randomData.year);
-        registrationPageTests.setSubjects(randomData.subjects);
-        registrationPageTests.setHobbies(randomData.hobbies);
-        registrationPageTests.setPicture(randomData.userPic);
-        registrationPageTests.setAddress(randomData.address);
-        registrationPageTests.setState(randomData.state);
-        registrationPageTests.setCity(randomData.city);
-        registrationPageTests.submit();
+        registrationPageTests.openPage()
+        .removeFixedbanFooter()
+        .setFirstName(randomData.firstName)
+        .setLastName(randomData.lastName)
+        .setUserEmail(randomData.userEmail)
+        .setGender(randomData.gender)
+        .setUserNumber(randomData.userNumber)
+        .setBirthDay(randomData.day, randomData.month, randomData.year)
+        .setSubjects(randomData.subjects)
+        .setHobbies(randomData.hobbies)
+        .setPicture(randomData.userPic)
+        .setAddress(randomData.address)
+        .setState(randomData.state)
+        .setCity(randomData.city)
+        .submit();
 
 
         resultPageComponent.checkOut("Student Name", randomData.firstName + " " + randomData.lastName);
         resultPageComponent.checkOut("Student Email", randomData.userEmail);
         resultPageComponent.checkOut("Gender", randomData.gender);
         resultPageComponent.checkOut("Mobile", randomData.userNumber);
-        resultPageComponent.checkOut("Date of Birth", randomData.day + " " + randomData.month + "," + randomData.year);
+        resultPageComponent.checkOut("Dat@e of Birth", randomData.day + " " + randomData.month + "," + randomData.year);
         resultPageComponent.checkOut("Subjects", randomData.subjects);
         resultPageComponent.checkOut("Hobbies", randomData.hobbies);
         resultPageComponent.checkOut("Picture", randomData.userPic);
