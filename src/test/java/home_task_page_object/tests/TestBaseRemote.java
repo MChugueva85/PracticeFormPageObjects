8b0c1a895ce787914a4cfa7ffd4f5ee20df16319
MChugueva85 <livingsmiling85@gmail.com>
2023-08-13 00:46:42 +0300

    added allure attachments

--- src/test/java/home_task_page_object/tests/TestBaseRemote.java
+++ src/test/java/home_task_page_object/tests/TestBaseRemote.java
@@ -1,7 +1,12 @@
 package home_task_page_object.tests;
 
 import com.codeborne.selenide.Configuration;
+import home_task_page_object.pages.helpers.Attach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBaseRemote {
 
@@ -11,5 +16,23 @@ public class TestBaseRemote {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
 
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
-}
+
+        @AfterEach
+        void addAttachments() {
+            Attach.screenshotAs("Last screenshot");
+            Attach.pageSource();
+            Attach.browserConsoleLogs();
+            Attach.addVideo();
+
+        }
+
+    }
