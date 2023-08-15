781fbea61acb021a98f8899c2f51d2e35984e000
MChugueva85 <livingsmiling85@gmail.com>
2023-08-15 23:25:49 +0300

    изменено название переменной selenoid в TestBaseRemote

--- src/test/java/home_task_page_object/tests/TestBaseRemote.java
+++ src/test/java/home_task_page_object/tests/TestBaseRemote.java
@@ -14,12 +14,12 @@ public class TestBaseRemote {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = System.getProperty("BASE_URL","https://demoqa.com");
+        Configuration.baseUrl = System.getProperty("baseUrl","https://demoqa.com");
         Configuration.browserSize = System.getProperty("browserSize","1920x1080");
         Configuration.pageLoadStrategy = "eager";
 
-        Configuration.remote = System.getProperty("remoteDriverUrl");
-        Configuration.browser = System.getProperty("browser", "opera");
+        Configuration.remote = System.getProperty("selenoid");
+        Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
